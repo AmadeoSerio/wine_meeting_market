234a08190bfd29cded0146a61903b2ae67016ce1
Amadeo Serio <amadeoserio@gmail.com>
2025-03-01 13:02:57 -0300

    eliminé del footer el mapa y la direccion

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,13 +10,13 @@ const Footer = () => {
 
         <p className='d-flex justify-content-center gap-3'>Comunicate con nosotras por WhatsApp <a target='blank' href="https://api.whatsapp.com/send/?phone=5492612051027&text&type=phone_number&app_absent=0"><i className="fa-brands fa-whatsapp"></i></a></p>
 
-        <p>Encontranos en Casa Amalfi 272 Emilio Civit Ciudad Mendoza.</p>
+        {/* <p>Encontranos en Casa Amalfi 272 Emilio Civit Ciudad Mendoza.</p> */}
 
-        <div className='footer-iframe'>
+        {/* <div className='footer-iframe'>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3350.373555002223!2d-68.8561615227597!3d-32.888290373617394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e09da653eab15%3A0x181b4713fc842770!2sCasa%20Amalfi!5e0!3m2!1ses!2sar!4v1715484067746!5m2!1ses!2sar"></iframe>
-        </div>
+        </div> */}
 
-        <p className='footer-final'>© 2024 Wine Meeting - Mendoza - Argentina</p>
+        <p className='footer-final'>© 2025 Wine Meeting - Mendoza - Argentina</p>
     </div>
     </section>
   )
